217f932899db28e242b9654fe869256dcea4218a
Vivian Yang <yangv473@newschool.edu>
2015-04-09 22:32:05 -0400

    type

---
+++ pattern.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+ 
+for (var multiplier = 1; multiplier < 10; multiplier++) {
+	console.log(multiplier*2);
+	var hands = "(=_=)";
+	for (var v = 0; v < 40; v++) {
+		faces = hands + "(=_=)";
+		console.log(hands);
+	}
+}
